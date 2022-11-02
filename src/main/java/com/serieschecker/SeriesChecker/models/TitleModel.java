4780bf5a93d906de508b8aa32b5899e62ec0ead8
kilokokaina <nikolaushki@gmail.com>
2022-11-02 23:21:31 +0800

    Temp commit

--- src/main/java/com/serieschecker/SeriesChecker/models/TitleModel.java
+++ src/main/java/com/serieschecker/SeriesChecker/models/TitleModel.java
@@ -10,17 +10,38 @@ import lombok.NoArgsConstructor;
 public class TitleModel {
     private String titleName;
     private String titleCountry;
-    private int titleYear;
-    private int titleSeasonNumber;
-    private int titleEpisodeDuration;
+    private String titlePlatform;
+    private String titleGenre;
+    private String titleYear;
+    private String titleSeasonNumber;
+    private String titleEpisodeDuration;
     private String titlePG;
+
+//    "Год производства", "Страна",
+//            "Платформа", "Жанр", "Возраст", "Время"
+
+    public void setFields(String fieldName, String fieldValue) {
+        switch (fieldName) {
+            case "Год производства" -> setTitleYear(fieldValue);
+            case "Страна" -> setTitleCountry(fieldValue);
+            case "Платформа" -> setTitlePlatform(fieldValue);
+            case "Жанр" -> setTitleGenre(fieldValue);
+            case "Возраст" -> setTitlePG(fieldValue);
+            case "Время" -> setTitleEpisodeDuration(fieldValue);
+        }
+    }
 
     @Override
     public String toString() {
         return String.format("""
                 Title: %s
-                Year: %d
+                Year: %s
                 Country: %s
-                """, getTitleName(), getTitleYear(), getTitleCountry());
+                Platform: %s
+                Genre: %s
+                PG: %s
+                Duration: %s
+                """, getTitleName(), getTitleYear(), getTitleCountry(), getTitlePlatform(),
+                getTitleGenre(), getTitlePG(), getTitleEpisodeDuration());
     }
 }
--- src/main/java/com/serieschecker/SeriesChecker/parser/KinopoiskParser.java
+++ src/main/java/com/serieschecker/SeriesChecker/parser/KinopoiskParser.java
@@ -1,5 +1,6 @@
 package com.serieschecker.SeriesChecker.parser;
 
+import com.serieschecker.SeriesChecker.models.TitleModel;
 import com.serieschecker.SeriesChecker.models.UserModel;
 import com.serieschecker.SeriesChecker.service.impl.UserServiceImpl;
 import lombok.extern.slf4j.Slf4j;
@@ -13,13 +14,19 @@ import org.springframework.stereotype.Component;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
 @Slf4j
 @Component
 public class KinopoiskParser implements CommandLineRunner {
-    private static final String sourceUrl = "/Users/nikol/Desktop/Kinopoisk/%s.html";
+    private static final String titleSourceUrl = "https://kinopoisk/";
+    private static final String linkSourceUrl = "/Users/nikol/Desktop/Kinopoisk/%s.html";
+
+    private static final String[] requiredFields = { "Год производства", "Страна",
+            "Платформа", "Жанр", "Возраст", "Время"
+    };
 
     private final UserServiceImpl userService;
 
@@ -35,12 +42,16 @@ public class KinopoiskParser implements CommandLineRunner {
         List<UserModel> userAdminList = userService.findByRole("admin");
         userAdminList.forEach(item -> log.info(item.toString()));
 
-        Document mainHTML = Jsoup.parse(new File(String.format(sourceUrl, "main")));
+        Document mainHTML = Jsoup.parse(new File(String.format(linkSourceUrl, "main")));
         Elements seriesLink = mainHTML.getElementsByClass("base-movie-main-info_link__YwtP1");
+
+        List<TitleModel> titleModelList = new ArrayList<>();
 
         int i = 0;
         for (Element link : seriesLink) {
-            if (i == 3) break;
+            if (i == 4) break;
+
+            TitleModel tempTitleModel = new TitleModel();
 
             String[] linkArray = link.attr("href").split("/");
             String titlePage = linkArray[linkArray.length - 1];
@@ -48,17 +59,26 @@ public class KinopoiskParser implements CommandLineRunner {
             try {
                 log.info("---------NEW PAGE---------");
 
-                Document titleHTML = Jsoup.parse(new File(String.format(sourceUrl, titlePage)));
-                log.info(Objects.requireNonNull(
-                        titleHTML.selectFirst("span[data-tid=2da92aed]")).text());
+                Document titleHTML = Jsoup.parse(new File(String.format(linkSourceUrl, titlePage)));
+                String titleTV = Objects.requireNonNull(
+                        titleHTML.selectFirst("span[data-tid=2da92aed]")).text();
 
-                Elements titleInfoDark = titleHTML.getElementsByClass(
-                        "styles_linkDark__7m929 styles_link__3QfAk");
-                Elements titleInfoLight = titleHTML.getElementsByClass(
-                        "styles_linkLight__cha3C styles_link__3QfAk");
+                tempTitleModel.setTitleName(titleTV);
 
-                titleInfoDark.forEach(info -> log.info(info.text()));
-                titleInfoLight.forEach(info -> log.info(info.text()));
+                Element divBody = Objects.requireNonNull(
+                        titleHTML.selectFirst("div[data-test-id=encyclopedic-table]"));
+
+                for (String field : requiredFields) {
+                    Elements divField = divBody.select(String.format(":containsOwn(%s)", field));
+                    Elements fieldsInfo = divField.next();
+
+                    if (divField.size() == 0) tempTitleModel.setFields(field, null);
+                    fieldsInfo.forEach(item ->
+                            tempTitleModel.setFields(field, item.text())
+                    );
+                }
+
+                titleModelList.add(tempTitleModel);
 
             } catch (IOException e) {
                 throw new RuntimeException(e);
@@ -66,5 +86,7 @@ public class KinopoiskParser implements CommandLineRunner {
 
             i++;
         }
+
+        titleModelList.forEach(item -> log.info(item.toString()));
     }
 }
